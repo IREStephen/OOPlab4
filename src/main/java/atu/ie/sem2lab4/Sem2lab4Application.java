30bf230e91b59579bdc11ae034139dcf38ea58ff
StephenDuffy-STUDENT <g00465649@atu.ie>
2026-02-25 14:58:18 +0000

    First Commit

---
+++ src/main/java/atu/ie/sem2lab4/Sem2lab4Application.java
@@ -0,0 +1,13 @@
+package atu.ie.sem2lab4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sem2lab4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sem2lab4Application.class, args);
+    }
+
+}
